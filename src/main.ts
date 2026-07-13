ee8122a61b30766e8251f5101d0f044310a92847
ehsan-ghanad <ghannad73@gmail.com>
2026-07-13 16:35:33 +0330

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import {AppModule} from "./app.module";
 import {ValidationPipe} from "@nestjs/common";
 import {PrismaExceptionFilter} from "./prisma-exception.filter";
 import {TransformInterceptor} from "./transform.interceptor";
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 (BigInt.prototype as any).toJSON = function () {
     return this.toString();
@@ -15,6 +16,10 @@ async function bootstrap() {
     app.useGlobalPipes(new ValidationPipe({whitelist: true}));
     app.useGlobalFilters(new PrismaExceptionFilter(httpAdapter));
     app.useGlobalInterceptors(new TransformInterceptor());
+
+    const config = new DocumentBuilder().setTitle("Task Manager API").setDescription("API documentation for the Task Manager").setVersion("1.0").build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("api", app, document);
     await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
