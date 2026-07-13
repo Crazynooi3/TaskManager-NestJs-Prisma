dcf8a0e0dfeedc507bc9e28fb1e14bd13d5cef9f
ehsan-ghanad <ghannad73@gmail.com>
2026-07-13 17:39:21 +0330

    create: member CRUD directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import {Module} from "@nestjs/common";
 import {ProjectsModule} from "./projects/projects.module";
 import {PrismaModule} from "./prisma.module";
+import { MemberModule } from './member/member.module';
 
 @Module({
-    imports: [ProjectsModule, PrismaModule]
+    imports: [ProjectsModule, PrismaModule, MemberModule]
 })
 export class AppModule {}
